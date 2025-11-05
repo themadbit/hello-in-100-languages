85df7f2dcc7fd7d85dcbb3c87161ac454394c3bb
themadbit <marktanui75@gmail.com>
2025-11-05 15:20:42 +0300

    add language: python3

---
+++ hello_python.py
@@ -0,0 +1,14 @@
+"""
+language: python(3)
+author: Guido van Rossum
+history & fun fact: First appeared in 1991.
+    Created by Van Rossum --who is no longer the BDFL, Python has been consitently ranked in the top ten most popular
+    programming languages since 2003.
+    The language derives its name not from the snake but from the British comedy series Monty Python's Flying Circus.
+    However, as in tradition of programming languages having an animal logo, Python's is a snake (python?).
+"""
+
+# hello world in python3
+# execution: python3 hello_python.py
+
+print("Hello, World :)")
