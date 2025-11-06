1d79a14535075ba8fe2a9953c1307c340f5e34ae
themadbit <marktanui75@gmail.com>
2025-11-06 11:31:58 +0300

    add language: ruby

---
+++ hello_ruby.rb
@@ -0,0 +1,9 @@
+=begin
+language: Ruby
+author: Yukihiro “Matz” Matsumoto
+history & fun fact: Official release was 1995. It was inspired by the author's favourite languages (Perl, Smalltalk, Eiffel, Ada, and Lisp).
+Unlike most other languages, its logo is a Ruby and not an animal mascot.
+=end
+
+# execution: ruby hello_ruby.rb
+puts "Hello, World :)"
