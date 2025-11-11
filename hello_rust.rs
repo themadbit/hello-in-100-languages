e351ce81bac8ce26a3284448c47cccc2033c43c0
themadbit <marktanui75@gmail.com>
2025-11-11 17:28:20 +0300

    add language: rust
    Signed-off-by: themadbit <marktanui75@gmail.com>

---
+++ hello_rust.rs
@@ -0,0 +1,13 @@
+/*
+* language: rust
+* author: Graydon Hoare
+* history and fun fact: Created in 2006 by Graydon -Mozilla staff at the time-, Rust was later released in 2015 after sponsorship by Mozilla for 6 years, since 2009.
+* During the early years of the language, the Rust Compiler was written in OCaml.
+* Rust has been adopted mainly for development of web services (browsers), and system software, including the Linux Kernel.
+*/
+
+// execution instructions:  rustc hello_rust.rs
+//                          ./hello_rust
+fn main() {
+    println!("Hello, World :)")
+}
