f0a62104080c77a589d667d61f94d714b55d9272
themadbit <marktanui75@gmail.com>
2025-11-13 23:59:40 +0300

    add language: ocaml

---
+++ hello_ocaml.ml
@@ -0,0 +1,6 @@
+(*
+language: ocaml
+author: (a couple of people actually) plus the most associated gug is Xavier Leroy
+history and fun fact: Ocaml first appeared in 1996. It is a multi-paradigm language -functional, imperative, modular and object-oriented- and a dialect of the ML language. It is statically typed, and the compiler has full type inference.
+*)
+let () = print_endline "Hello, World :)"
