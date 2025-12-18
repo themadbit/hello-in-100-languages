d05ae3b0e65859be88f3a78027f39b5ec28a5c8a
themadbit <marktanui75@gmail.com>
2025-12-18 17:25:13 +0300

    add language: go

---
+++ hello_go.go
@@ -0,0 +1,19 @@
+/*
+language: go
+author: Designed at Google by great Computer Scientists, 	Robert Griesemer, Rob Pike, Ken Thompson.
+history & fun fact: Go was officially release to the public in Nov 2009. The first ever public issue ticket was a naming dispute
+by Francis McCabe, an author of a language named Go! The issue was closed 11 months later, with the label
+'unfortunate'. see: "https://en.wikipedia.org/wiki/Go_(programming_language)#Naming_dispute"
+*/
+
+/*
+building: go build hello.go
+execution: ./hello
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World :)")
+}
